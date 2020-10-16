bffd8f8637ed7aaae6bd3689991b71c2c7660e11
krzys <krzysztof.skro@gmail.com>
2020-10-16 20:51:58 +0200

    Added sharpenes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 from numpy import round
 from math import floor
 
+#font Libertum mono
+
 SCALE = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. """
-MAX_RESOLUTION = (512,512)
+MAX_RESOLUTION = (300,300)
 
 def px2char(p): #return corresponded char of given pixel
 
@@ -18,25 +20,41 @@ def preproces(img):#prepare image to processing
         img = img.resize((MAX_RESOLUTION[0], int((MAX_RESOLUTION[0]/width)*height)))
     if height > MAX_RESOLUTION[1]:
         img = img.resize((int((MAX_RESOLUTION[1]/height) * width), MAX_RESOLUTION[1]))
+
+    img2letters(img, f"./out.txt")
+
+    enhancer = ImageEnhance.Sharpness(img)
+    factor = 36
+    img =enhancer.enhance(factor)
+    img.show()
+    img2letters(img, f"./out2.txt")
+
     return img
 
-src = f"./image.jpg"
+def img2letters(img, path):
+    width, height = img.size
+    px = img.load()
+    file = open(path, "w")
+    for y in range(height):
+        for x in range(width):
+            file.write(px2char(px[x, y]))
+        file.write("\n")
+
+    file.close()
+
+src = f"./images/image2.jpg"
 img = Image.open(src).convert("L") #open in graySCALE
+
 
 img = preproces(img)
 
 
-width, height = img.size
-px = img.load()
-file = open("./out.txt", "w")
 
 
-for y in range(height):
-     for x in range(width):
-        file.write(px2char(px[x,y]))
-     file.write("\n")
+
+
+
 
 #img.show()
 print("Process complete")
 img.close()
-file.close()
