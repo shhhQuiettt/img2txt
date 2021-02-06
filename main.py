2a460d49a9caa37970c0c108ca1a59c2595c5449
krzys <krzysztof.skro@gmail.com>
2021-02-06 21:14:06 +0100

    img2txt as a terminal programm
    python main.py [source] [,--scale xxy]
    saves to name.txt in this same direction

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 from PIL import Image, ImageEnhance
 from numpy import round
-from math import floor
+import sys
+import os
 
 #font Libertum mono
 
+arg = sys.argv
+
+
+S_PATH = arg[1]
+filename, ext = os.path.splitext(S_PATH)
+
+# D_PATH= arg[2]
+arg = arg[2:]
+
+print(arg)
 SCALE = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. """
-MAX_RESOLUTION = (300,300)
-S_PATH = f"./images/image2.jpg"
-D_PATH = f"./outs/out.txt"
+
+if "--scale" in arg:
+    m_r = arg[arg.index("--scale")+1].split('x')
+    for i in range(2):
+        if m_r[i] == 'X':
+            m_r[i] = None
+        else:
+            m_r[i] = int(m_r[i])
+        MAX_RESOLUTION = tuple(m_r)
+
+else:
+    MAX_RESOLUTION = (None,256)
+
+print(MAX_RESOLUTION)
+# S_PATH = f"./images/crew.jpg"
+# D_PATH = f"./outs/crewtxttxt"
 
 def px2char(p): #return corresponded char of given pixel
     n = len(SCALE)
@@ -15,32 +39,45 @@ def px2char(p): #return corresponded char of given pixel
     return SCALE[sc]
 
 def preproces(img):#prepare image to processing
-    (width, height) = img.size
+    width , height = img.size
+    img = img.resize((int(1.41*width),height))
 
-    if width > MAX_RESOLUTION[0]:
+    width, height = img.size
+    if MAX_RESOLUTION[0] != None and width > MAX_RESOLUTION[0]:
         img = img.resize((MAX_RESOLUTION[0], int((MAX_RESOLUTION[0]/width)*height)))
-    if height > MAX_RESOLUTION[1]:
+
+    width, height = img.size
+    if MAX_RESOLUTION[1] != None and height > MAX_RESOLUTION[1]:
         img = img.resize((int((MAX_RESOLUTION[1]/height) * width), MAX_RESOLUTION[1]))
 
     enhancer = ImageEnhance.Sharpness(img)
-    factor = 36
+    factor = 25
     img =enhancer.enhance(factor)
+   # img.show()
+
 
     return img
 
-def img2letters(img, D_PATH): #create out file with letter instead of pixels
+def img2letters(img, filename): #create out file with letter instead of pixels
     width, height = img.size
+
     px = img.load()
-    file = open(D_PATH, "w")
+    file = open(filename+".txt", "w")
     for y in range(height):
         for x in range(width):
-            file.write(px2char(px[x, y]))
+            if (x%2 == 0 and y%2 ==0) or (x%2==1 and y%2 ==1):
+              #  last = px2char(px[x, y])
+              #  file.write(last)
+                file.write(px2char(px[x,y]))
+            else:
+                file.write(px2char(px[x, y]))
+
         file.write("\n")
     file.close()
 
 img = Image.open(S_PATH).convert("L") #open in graySCALE
 img = preproces(img)    #resizing, sharpness
-img2letters(img, D_PATH) #main action
+img2letters(img, filename) #main action
 
 print("Process complete")
 
